e594d4dba61fc91fa86e70af1fd9aaef0531fec4
brayhoward <brandonrhoward@gmail.com>
2019-04-19 09:44:36 -0500

    Initial commit.

---
+++ core/MaterialTypography/index.d.ts
@@ -0,0 +1,2 @@
+export { default } from "./MaterialTypography";
+export * from "./MaterialTypography";
---
+++ core/index.js
@@ -0,0 +1,299 @@
+/** @license Material-UI v3.9.3
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+"use strict";
+
+var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
+
+var _interopRequireWildcard = require("@babel/runtime/helpers/interopRequireWildcard");
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "createGenerateClassName", {
+  enumerable: true,
+  get: function get() {
+    return _styles.createGenerateClassName;
+  }
+});
+Object.defineProperty(exports, "createMuiTheme", {
+  enumerable: true,
+  get: function get() {
+    return _styles.createMuiTheme;
+  }
+});
+Object.defineProperty(exports, "createStyles", {
+  enumerable: true,
+  get: function get() {
+    return _styles.createStyles;
+  }
+});
+Object.defineProperty(exports, "jssPreset", {
+  enumerable: true,
+  get: function get() {
+    return _styles.jssPreset;
+  }
+});
+Object.defineProperty(exports, "MuiThemeProvider", {
+  enumerable: true,
+  get: function get() {
+    return _styles.MuiThemeProvider;
+  }
+});
+Object.defineProperty(exports, "withStyles", {
+  enumerable: true,
+  get: function get() {
+    return _styles.withStyles;
+  }
+});
+Object.defineProperty(exports, "withTheme", {
+  enumerable: true,
+  get: function get() {
+    return _styles.withTheme;
+  }
+});
+
+Object.defineProperty(exports, "SnackbarContent", {
+  enumerable: true,
+  get: function get() {
+    return _SnackbarContent.default;
+  }
+});
+
+Object.defineProperty(exports, "Snackbar", {
+  enumerable: true,
+  get: function get() {
+    return _Snackbar.default;
+  }
+});
+exports.colors = void 0;
+
+var colors = _interopRequireWildcard(require("./colors"));
+
+exports.colors = colors;
+
+var _styles = require("./styles");
+
+var _AppBar = _interopRequireDefault(require("./AppBar"));
+
+var _Avatar = _interopRequireDefault(require("./Avatar"));
+
+var _Backdrop = _interopRequireDefault(require("./Backdrop"));
+
+var _Badge = _interopRequireDefault(require("./Badge"));
+
+var _BottomNavigation = _interopRequireDefault(require("./BottomNavigation"));
+
+var _BottomNavigationAction = _interopRequireDefault(
+  require("./BottomNavigationAction")
+);
+
+var _Button = _interopRequireDefault(require("./Button"));
+
+var _ButtonBase = _interopRequireDefault(require("./ButtonBase"));
+
+var _Card = _interopRequireDefault(require("./Card"));
+
+var _CardActionArea = _interopRequireDefault(require("./CardActionArea"));
+
+var _CardActions = _interopRequireDefault(require("./CardActions"));
+
+var _CardContent = _interopRequireDefault(require("./CardContent"));
+
+var _CardHeader = _interopRequireDefault(require("./CardHeader"));
+
+var _CardMedia = _interopRequireDefault(require("./CardMedia"));
+
+var _Checkbox = _interopRequireDefault(require("./Checkbox"));
+
+var _Chip = _interopRequireDefault(require("./Chip"));
+
+var _CircularProgress = _interopRequireDefault(require("./CircularProgress"));
+
+var _ClickAwayListener = _interopRequireDefault(require("./ClickAwayListener"));
+
+var _Collapse = _interopRequireDefault(require("./Collapse"));
+
+var _CssBaseline = _interopRequireDefault(require("./CssBaseline"));
+
+var _Dialog = _interopRequireDefault(require("./Dialog"));
+
+var _DialogActions = _interopRequireDefault(require("./DialogActions"));
+
+var _DialogContent = _interopRequireDefault(require("./DialogContent"));
+
+var _DialogContentText = _interopRequireDefault(require("./DialogContentText"));
+
+var _DialogTitle = _interopRequireDefault(require("./DialogTitle"));
+
+var _Divider = _interopRequireDefault(require("./Divider"));
+
+var _Drawer = _interopRequireDefault(require("./Drawer"));
+
+var _ExpansionPanel = _interopRequireDefault(require("./ExpansionPanel"));
+
+var _ExpansionPanelActions = _interopRequireDefault(
+  require("./ExpansionPanelActions")
+);
+
+var _ExpansionPanelDetails = _interopRequireDefault(
+  require("./ExpansionPanelDetails")
+);
+
+var _ExpansionPanelSummary = _interopRequireDefault(
+  require("./ExpansionPanelSummary")
+);
+
+var _Fab = _interopRequireDefault(require("./Fab"));
+
+var _Fade = _interopRequireDefault(require("./Fade"));
+
+var _FilledInput = _interopRequireDefault(require("./FilledInput"));
+
+var _FormControl = _interopRequireDefault(require("./FormControl"));
+
+var _FormControlLabel = _interopRequireDefault(require("./FormControlLabel"));
+
+var _FormGroup = _interopRequireDefault(require("./FormGroup"));
+
+var _FormHelperText = _interopRequireDefault(require("./FormHelperText"));
+
+var _FormLabel = _interopRequireDefault(require("./FormLabel"));
+
+var _Grid = _interopRequireDefault(require("./Grid"));
+
+var _GridList = _interopRequireDefault(require("./GridList"));
+
+var _GridListTile = _interopRequireDefault(require("./GridListTile"));
+
+var _GridListTileBar = _interopRequireDefault(require("./GridListTileBar"));
+
+var _Grow = _interopRequireDefault(require("./Grow"));
+
+var _Hidden = _interopRequireDefault(require("./Hidden"));
+
+var _Icon = _interopRequireDefault(require("./Icon"));
+
+var _IconButton = _interopRequireDefault(require("./IconButton"));
+
+var _Input = _interopRequireDefault(require("./Input"));
+
+var _InputAdornment = _interopRequireDefault(require("./InputAdornment"));
+
+var _InputBase = _interopRequireDefault(require("./InputBase"));
+
+var _InputLabel = _interopRequireDefault(require("./InputLabel"));
+
+var _LinearProgress = _interopRequireDefault(require("./LinearProgress"));
+
+var _Link = _interopRequireDefault(require("./Link"));
+
+var _List = _interopRequireDefault(require("./List"));
+
+var _ListItem = _interopRequireDefault(require("./ListItem"));
+
+var _ListItemAvatar = _interopRequireDefault(require("./ListItemAvatar"));
+
+var _ListItemIcon = _interopRequireDefault(require("./ListItemIcon"));
+
+var _ListItemSecondaryAction = _interopRequireDefault(
+  require("./ListItemSecondaryAction")
+);
+
+var _ListItemText = _interopRequireDefault(require("./ListItemText"));
+
+var _ListSubheader = _interopRequireDefault(require("./ListSubheader"));
+
+var _Menu = _interopRequireDefault(require("./Menu"));
+
+var _MenuItem = _interopRequireDefault(require("./MenuItem"));
+
+var _MenuList = _interopRequireDefault(require("./MenuList"));
+
+var _MobileStepper = _interopRequireDefault(require("./MobileStepper"));
+
+var _Modal = _interopRequireWildcard(require("./Modal"));
+
+var _NativeSelect = _interopRequireDefault(require("./NativeSelect"));
+
+var _NoSsr = _interopRequireDefault(require("./NoSsr"));
+
+var _OutlinedInput = _interopRequireDefault(require("./OutlinedInput"));
+
+var _Paper = _interopRequireDefault(require("./Paper"));
+
+var _Popover = _interopRequireDefault(require("./Popover"));
+
+var _Popper = _interopRequireDefault(require("./Popper"));
+
+var _Portal = _interopRequireDefault(require("./Portal"));
+
+var _Radio = _interopRequireDefault(require("./Radio"));
+
+var _RadioGroup = _interopRequireDefault(require("./RadioGroup"));
+
+var _RootRef = _interopRequireDefault(require("./RootRef"));
+
+var _Select = _interopRequireDefault(require("./Select"));
+
+var _Slide = _interopRequireDefault(require("./Slide"));
+
+var _Snackbar = _interopRequireDefault(require("./Snackbar"));
+
+var _SnackbarContent = _interopRequireDefault(require("./SnackbarContent"));
+
+var _Step = _interopRequireDefault(require("./Step"));
+
+var _StepButton = _interopRequireDefault(require("./StepButton"));
+
+var _StepConnector = _interopRequireDefault(require("./StepConnector"));
+
+var _StepContent = _interopRequireDefault(require("./StepContent"));
+
+var _StepIcon = _interopRequireDefault(require("./StepIcon"));
+
+var _StepLabel = _interopRequireDefault(require("./StepLabel"));
+
+var _Stepper = _interopRequireDefault(require("./Stepper"));
+
+var _SvgIcon = _interopRequireDefault(require("./SvgIcon"));
+
+var _SwipeableDrawer = _interopRequireDefault(require("./SwipeableDrawer"));
+
+var _Switch = _interopRequireDefault(require("./Switch"));
+
+var _Tab = _interopRequireDefault(require("./Tab"));
+
+var _Table = _interopRequireDefault(require("./Table"));
+
+var _TableBody = _interopRequireDefault(require("./TableBody"));
+
+var _TableCell = _interopRequireDefault(require("./TableCell"));
+
+var _TableFooter = _interopRequireDefault(require("./TableFooter"));
+
+var _TableHead = _interopRequireDefault(require("./TableHead"));
+
+var _TablePagination = _interopRequireDefault(require("./TablePagination"));
+
+var _TableRow = _interopRequireDefault(require("./TableRow"));
+
+var _TableSortLabel = _interopRequireDefault(require("./TableSortLabel"));
+
+var _Tabs = _interopRequireDefault(require("./Tabs"));
+
+var _TextField = _interopRequireDefault(require("./TextField"));
+
+var _Toolbar = _interopRequireDefault(require("./Toolbar"));
+
+var _Tooltip = _interopRequireDefault(require("./Tooltip"));
+
+var _Typography = _interopRequireDefault(require("./Typography"));
+
+var _withMobileDialog = _interopRequireDefault(require("./withMobileDialog"));
+
+var _withWidth = _interopRequireDefault(require("./withWidth"));
+
+var _Zoom = _interopRequireDefault(require("./Zoom"));
